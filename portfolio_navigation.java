009fe17da4e00109255469d2eba18a346e6bb1bc
GimelYoung <gimelyoung@gmail.com>
2017-02-28 09:13:59 -0500

    First Virsion

---
+++ portfolio_navigation.java
@@ -0,0 +1,7 @@
+function toggle_visibility(logos) {
+	var e = document.getElementById(flyers, instructional, business, websites, misc);
+	if(e.style.display == 'none')
+	e.style.display = 'block';
+	else
+	e.style.display = 'none';
+}
